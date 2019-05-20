695846ef691c11a494188e6172452def1f658d55
Mehmet Tek <mehmet_tek@windowslive.com>
2019-05-20 22:07:40 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ales, uds, kpds, i, sn;
+            Console.Write("ALES NOTUNUZU  GİRİNİZ");
+            ales = Convert.ToInt32(Console.ReadLine());
+            if ((ales >= 60) && (ales <= 100))
+            {
+                while (true)
+                {
+                    Console.WriteLine("BÖLÜM KODUNUZU GİRİNİZ>>>");
+
+                    Console.Write("İNGİLİZCE İCİN  1  TÜRKCE İÇİN 0 GİRİNİZ");
+                    i = Convert.ToInt32(Console.ReadLine());
+                    if (i == 1)
+                    {
+                        Console.Write("ÜDS NOTUNUZU GİRİNİZ>>>");
+                        uds = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("KPDS NOTUNUZU GİRİNİZ>>");
+                        kpds = Convert.ToInt32(Console.ReadLine());
+                        if ((uds >= 60) && (kpds >= 60))
+                        {
+                            Console.Write("LÜTFEN ÜNİVERSİTEMİZİN SINAVINDAN ALDIGINIZ NOTU GİRİNİZ>");
+                            sn = Convert.ToInt32(Console.ReadLine());
+                            if ((sn >= 60) && (sn <= 100))
+                            {
+                                Console.Write("TEBRİKLER ÜNİVERSİTEMİZDE YÜKSEK LİSANS YAPABİLİRSİNİZ  ");
+                                Console.Read();
+                                break;
+                            }
+                            else
+                            {
+                                Console.Write("BASVURUNUZ RED EDİLMİSTİR");
+                                Console.Read();
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("UDS VEYA KPDS NOTUNUZ YÜZÜNDEN OLMADI");
+                            Console.Read();
+                            break;
+                        }
+                    }
+                    else if (i == 0)
+                    {
+
+                        Console.Write("ÜDS NOTUNUZU GİRİNİZ>>>");
+                        uds = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("KPDS NOTUNUZU GİRİNİZ>>");
+                        kpds = Convert.ToInt32(Console.ReadLine());
+                        if ((uds >= 30) && (kpds >= 30))
+                        {
+                            Console.Write("LÜTFEN ÜNİVERSİTEMİZİN SINAVINDAN ALDIGINIZ NOTU GİRİNİZ>");
+                            sn = Convert.ToInt32(Console.ReadLine());
+                            if ((sn >= 60) && (sn <= 100))
+                            {
+                                Console.Write("TEBRİKLER ÜNİVERSİTEMİZDE YÜKSEK LİSANS YAPABİLİRSİNİZ ");
+                                Console.Read();
+                                break;
+                            }
+                            else
+                            {
+                                Console.Write("BASVURUNUZ RED EDİLMİSTİR");
+                                Console.Read();
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("UDS VEYA KPDS NOTUNUZ YÜZÜNDEN OLMADI");
+                            Console.Read();
+                            break;
+                        }
+
+                    }
+
+                    else
+                    {
+                        Console.Write("LÜTFEN OKUL KODUNUZU DOGRU GİRİNİZ");
+
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("ALES PUANINIZ YÜZÜDEN YÜKSEK LİSANS YAPAMAZSINIZ");
+                Console.Read();
+            }
+        }
+    }
+
+}
